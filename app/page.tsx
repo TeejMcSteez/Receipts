004b344cd23d055bb4489b46fe18dfcde0205a67
TeejMcSteez <teejmcsteez@gmail.com>
2025-07-13 11:48:12 -0400

    Update homepage styles and add search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 
 export default function Home() {
   return (
-    <div className="flex justify-between">
-      <h1 className="m-2 p-1">Reciepts</h1>
-      <Navbar />
+    <div className="">
+      <div className="flex justify-between">
+        <h1 className="m-2 p-1 text-xl">Reciepts</h1>
+        <Navbar />
+      </div>
+      <div className="flex items-center justify-center">
+        <Search />
+      </div>
     </div>
   );
 }
