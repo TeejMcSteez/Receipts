08f3eecf26cd6f8a54f5937e32e7a202434737ef
TeejMcSteez <teejmcsteez@gmail.com>
2025-07-13 17:15:53 -0400

    Add about page

---
+++ app/about/Why.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function Why() {
+    return(
+        <div className="flex flex-col items-center justify-center m-2 p-2">
+            <h1>This is a website made by a University Computer Science Student, mainly to collect information about topics that they find interesting (Especially in law and business)</h1>
+            <h2>This should not be misconstrude as some mission or overarching goal it is simply a library with information like any their are good and bad books</h2>
+            <h2>However, It is of a great obligation for the creator of the website to maintain factual information as otherwise this would basically be a blog not a library</h2>
+            <h2>In doing so their are interfaces in which other people can add their information which will go under review by the creator and maybe others not quite sure how that will work yet so it is not only information I collect but anyone else as well. A true library!</h2>
+            <h3>Their will be many updates and improvements and more than likely mistakes but the goal is open, unfettered, and objective factual access to information like many others want.</h3>
+        </div>
+    );
+}
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Why from "./Why";
 
 export default function About() {
     return(
         <>
             <Navbar />
+            <Why />
         </>
     );
 }
